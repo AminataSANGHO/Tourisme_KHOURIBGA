41bc987402fccf546e9c76d91543876e5c53c611
AminataSANGHO <tanamia1900@gmail.com>
2023-04-26 20:14:34 +0100

    Banque page

--- src/main/java/com/projet/Controller/indexController.java
+++ src/main/java/com/projet/Controller/indexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class indexController {
 	
-	@GetMapping("/index")
+	@GetMapping(value={"/", "/index"})
     public String index() {
         return "index"; 
     }
